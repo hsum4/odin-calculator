e6e66d0bed79e8f97443bfde078c3ecc42506754
hsum4 <93267846+hsum4@users.noreply.github.com>
2024-11-23 13:23:53 +0800

    create operator function

--- javascript.js
+++ javascript.js
@@ -1,5 +1,6 @@
-// functions
 
+
+// functions
 //add function
 function add (firstNum, secondNum){
     return firstNum + secondNum;
@@ -18,4 +19,18 @@ function multiply (firstNum, secondNum){
 //divide function
 function divide (firstNum, secondNum){
     return firstNum / secondNum;
+}
+
+//operator function
+function operator (operator, firstNum, secondNum) {
+    switch (operator) {
+        case "+":
+            return add(firstNum, secondNum)
+        case "-":
+            return substract(firstNum, secondNum)
+        case "*":
+            return multiply(firstNum, secondNum)
+        case "/":
+            return diivide(firstNum, secondNum)
+    }
 }
